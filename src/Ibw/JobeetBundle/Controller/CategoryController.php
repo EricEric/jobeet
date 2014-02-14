043ef19fb761cbcae9c5aebef311694f1ea1f45a
Eric Yu <eric.yu@soliton-music.com>
2014-02-14 18:34:21 +0800

    add Category Page

--- src/Ibw/JobeetBundle/Controller/CategoryController.php
+++ src/Ibw/JobeetBundle/Controller/CategoryController.php
@@ -11,4 +11,20 @@ use Ibw\JobeetBundle\Entity\Category;
 class CategoryController extends Controller
 {
 
+    public function showAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $category = $em->getRepository('IbwJobeetBundle:Category')->findOneBySlug($slug);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $category->setActiveJobs($em->getRepository('IbwJobeetBundle:Job')->getActiveJobs($category->getId()));
+
+        return $this->render('IbwJobeetBundle:Category:show.html.twig', array(
+            'category' => $category,
+        ));
+    }
 }
