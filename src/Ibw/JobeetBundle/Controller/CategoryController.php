53bf4df692a7dc006a50fdf17698f991377969d0
Eric Yu <eric.yu@soliton-music.com>
2014-02-14 18:19:41 +0800

    add CategoryController

---
+++ src/Ibw/JobeetBundle/Controller/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ibw\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ibw\JobeetBundle\Entity\Category;
+
+/**
+ * Category controller
+ *
+ */
+class CategoryController extends Controller
+{
+
+}
