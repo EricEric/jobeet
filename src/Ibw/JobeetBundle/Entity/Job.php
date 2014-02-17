bd37a237c749abad084f896a9ce0b374ef038fbf
Eric Yu <eric.yu@soliton-music.com>
2014-02-17 18:57:24 +0800

    Job Activation and Publication

--- src/Ibw/JobeetBundle/Entity/Job.php
+++ src/Ibw/JobeetBundle/Entity/Job.php
@@ -612,4 +612,9 @@ class Job
     {
         return ceil(($this->getExpiresAt()->format('U') - time()) / 86400);
     }
+
+    public function publish()
+    {
+        $this->setIsActivated(true);
+    }
 }
