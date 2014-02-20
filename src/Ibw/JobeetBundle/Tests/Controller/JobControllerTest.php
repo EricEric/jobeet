4589ed6bd786e8031ef35341739918d24d12a7e6
Eric Yu <eric.yu@soliton-music.com>
2014-02-20 15:44:29 +0800

    test submitting a form

--- src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -134,4 +134,28 @@ class JobControllerTest extends WebTestCase
         $crawler = $client->request('GET', sprintf('/job/sensio-labs/paris-france/%d/web-developer', $this->getExpiredJob()->getId()));
         $this->assertTrue(404 === $client->getResponse()->getStatusCode());
     }
+
+    public function testJobForm()
+    {
+        $client = static::createClient();
+
+        //test submitting a form
+        $crawler = $client->request('GET', '/job/new');
+        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::newAction', $client->getRequest()->attributes->get('_controller'));
+
+        $form = $crawler->selectButton('Preview your job')->form(array(
+            'job[company]'      => 'Sensio Labs',
+            'job[url]'          => 'http://www.sensio.com/',
+            'job[file]'         => __DIR__.'/../../../../../web/bundles/ibwjobeet/images/sensio-labs.gif',
+            'job[position]'     => 'Developer',
+            'job[location]'     => 'Atlanta, USA',
+            'job[description]'  => 'You will work with symfony to develop websites for our customers.',
+            'job[how_to_apply]' => 'Send me an email',
+            'job[email]'        => 'for.a.job@example.com',
+            'job[is_public]'    => false,
+        ));
+
+        $client->submit($form);
+        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::createAction', $client->getRequest()->attributes->get('_controller'));
+    }
 }
