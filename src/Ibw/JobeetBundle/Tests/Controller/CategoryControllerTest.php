5556230407d56eeb0be2e52681569307641d83e1
Eric Yu <eric.yu@soliton-music.com>
2014-02-20 14:04:07 +0800

    update LEARN BY THE EXAMPLE

--- src/Ibw/JobeetBundle/Tests/Controller/CategoryControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/CategoryControllerTest.php
@@ -9,11 +9,10 @@ use Doctrine\Bundle\DoctrineBundle\Command\DropDatabaseDoctrineCommand;
 use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
 use Doctrine\Bundle\DoctrineBundle\Command\Proxy\CreateSchemaDoctrineCommand;
 
-class JobControllerTest extends WebTestCase
+class CategoryControllerTest extends WebTestCase
 {
     private $em;
     private $application;
-
     public function setUp()
     {
         static::$kernel = static::createKernel();
@@ -66,72 +65,63 @@ class JobControllerTest extends WebTestCase
         $executor->execute($loader->getFixtures());
     }
 
-    public function getMostRecentProgrammingJob()
+    public function testShow()
     {
         $kernel = static::createKernel();
         $kernel->boot();
-        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
-        $query = $em->createQuery('SELECT j from IbwJobeetBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expires_at > :date ORDER BY j.created_at DESC');
-        $query->setParameter('slug', 'programming');
-        $query->setParameter('date', date('Y-m-d H:i:s', time()));
-        $query->setMaxResults(1);
-
-        return $query->getSingleResult();
-    }
-
-    public function getExpiredJob()
-    {
-        $kernel = static::createKernel();
-        $kernel->boot();
-        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-
-        $query = $em->createQuery('SELECT j from IbwJobeetBundle:Job j WHERE j.expires_at < :date');
-        $query->setParameter('date', date('Y-m-d H:i:s', time()));
-        $query->setMaxResults(1);
-
-        return $query->getSingleResult();
-    }
-
-    public function testIndex()
-    {
-        // get the custom parameters from app config.yml
-        $kernel = static::createKernel();
-        $kernel->boot();
+        // get the custom parameters from app/config.yml
+        $max_jobs_on_category = $kernel->getContainer()->getParameter('max_jobs_on_category');
         $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
 
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::indexAction', $client->getRequest()->attributes->get('_controller'));
+        $categories = $this->em->getRepository('IbwJobeetBundle:Category')->getWithJobs();
 
-        // expired jobs are not listed
-        $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0);
+        // categories on homepage are clickable
+        foreach($categories as $category) {
+            $crawler = $client->request('GET', '/');
 
-        // only $max_jobs_on_homepage jobs are listed for a category
-        $this->assertTrue($crawler->filter('.category_programming tr')->count()<= $max_jobs_on_homepage);
-        $this->assertTrue($crawler->filter('.category_design .more_jobs')->count() == 0);
-        $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
+            $link = $crawler->selectLink($category->getName())->link();
+            $crawler = $client->click($link);
 
-        // jobs are sorted by date
-        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
+            $this->assertEquals('Ibw\JobeetBundle\Controller\CategoryController::showAction', $client->getRequest()->attributes->get('_controller'));
+            $this->assertEquals($category->getSlug(), $client->getRequest()->attributes->get('slug'));
 
-        // each job on the homepage is clickable and give detailed information
-        $job = $this->getMostRecentProgrammingJob();
-        $link = $crawler->selectLink('Web Developer')->first()->link();
-        $crawler = $client->click($link);
-        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::showAction', $client->getRequest()->attributes->get('_controller'));
-        $this->assertEquals($job->getCompanySlug(), $client->getRequest()->attributes->get('company'));
-        $this->assertEquals($job->getLocationSlug(), $client->getRequest()->attributes->get('location'));
-        $this->assertEquals($job->getPositionSlug(), $client->getRequest()->attributes->get('position'));
-        $this->assertEquals($job->getId(), $client->getRequest()->attributes->get('id'));
+            $jobs_no = $this->em->getRepository('IbwJobeetBundle:Job')->countActiveJobs($category->getId());
 
-        // a non-existent job forwards the user to a 404
-        $crawler = $client->request('GET', '/job/foo-inc/milano-italy/0/painter');
-        $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+            // categories with more than $max_jobs_on_homepage jobs also have a "more" link
+            if($jobs_no > $max_jobs_on_homepage) {
+                $crawler = $client->request('GET', '/');
+                $link = $crawler->filter(".category_" . $category->getSlug() . " .more_jobs a")->link();
+                $crawler = $client->click($link);
 
-        // an expired job page forwards the user to a 404
-        $crawler = $client->request('GET', sprintf('/job/sensio-labs/paris-france/%d/web-developer', $this->getExpiredJob()->getId()));
-        $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+                $this->assertEquals('Ibw\JobeetBundle\Controller\CategoryController::showAction', $client->getRequest()->attributes->get('_controller'));
+                $this->assertEquals($category->getSlug(), $client->getRequest()->attributes->get('slug'));
+            }
+
+            $pages = ceil($jobs_no/$max_jobs_on_category);
+
+            // only $max_jobs_on_category jobs are listed
+            $this->assertTrue($crawler->filter('.jobs tr')->count() <= $max_jobs_on_category);
+            $this->assertRegExp("/" . $jobs_no . " jobs/", $crawler->filter('.pagination_desc')->text());
+
+            if($pages > 1) {
+                $this->assertRegExp("/page 1\/" . $pages . "/", $crawler->filter('.pagination_desc')->text());
+
+                for ($i = 2; $i <= $pages; $i++) {
+                    $link = $crawler->selectLink($i)->link();
+                    $crawler = $client->click($link);
+
+                    $this->assertEquals('Ibw\JobeetBundle\Controller\CategoryController::showAction', $client->getRequest()->attributes->get('_controller'));
+                    $this->assertEquals($i, $client->getRequest()->attributes->get('page'));
+                    $this->assertTrue($crawler->filter('.jobs tr')->count() <= $max_jobs_on_category);
+                    if($jobs_no >1) {
+                        $this->assertRegExp("/" . $jobs_no . " jobs/", $crawler->filter('.pagination_desc')->text());
+                    }
+                    $this->assertRegExp("/page " . $i . "\/" . $pages . "/", $crawler->filter('.pagination_desc')->text());
+                }
+            }
+        }
     }
 }
